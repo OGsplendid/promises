c7023a3a1cf3673d91cc2ebf27480a5762aa53a7
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-06-01 20:11:22 +0300

    fixes done

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -3,8 +3,9 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      resolve(json(read()));
-    });
+    return read()
+      .then((response) => json((response)))
+      .then((data) => JSON.parse(data))
+      .catch((err) => new Error(err));
   }
 }
