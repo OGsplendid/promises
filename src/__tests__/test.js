f64d5be1889bd7739fc7db94174fff476fe7093a
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-06-01 20:27:29 +0300

    fixes done

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,20 +1,19 @@
 import GameSavingLoader from '../GameSavingLoader';
 
-// jest.setTimeout(10000);
-
 test('should return correct object', () => {
   return GameSavingLoader.load()
-  .then((data) => {
-    expect(data).toEqual(
-      {
-        id: 9,
-        created: 1546300800,
-        userInfo: {
-          id: 1,
-          name: 'Hitman',
-          level: 10,
-          points: 2000,
+    .then((data) => {
+      expect(data).toEqual(
+        {
+          id: 9,
+          created: 1546300800,
+          userInfo: {
+            id: 1,
+            name: 'Hitman',
+            level: 10,
+            points: 2000,
+          },
         },
-      });
-  });
+      );
+    });
 });
--- src/app.js
+++
@@ -1,3 +0,0 @@
-import GameSavingLoader from './GameSavingLoader';
-
-GameSavingLoader.load().then((saving) => console.log(saving)).catch((err) => console.log(err));
